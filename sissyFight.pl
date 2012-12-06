2f8c1c3cb3b23aa8de499b145491b8c36e2bc3da
James Clemer <clemer19@gmail.com>
2012-12-06 16:52:06 -0600

    WIP of sissyFight: Mean Girls the Card Game the Video Game the Bot.

---
+++ sissyFight.pl
@@ -0,0 +1,102 @@
+% co-authored by Matthew Drahozal, James Clemer
+% Consultancy: Cait Hakala, on the nature of Bitchiness
+% 10 self-esteem points
+% 1. public communication
+% 2. note-passing?
+% 3. choose action:
+% -> Verbal Attack
+% -> Group Attack
+% -> Defend
+
+% representing social momentum
+% there's always this one girl no matter the social group, that everyone wants to be friends with. People are constantly fighting for the top girl, no matter her worth as a prize.
+% some girls turn bitchy, some do not
+
+agent(veronicaMars).
+self_esteem(veronicaMars, 25).
+current_action(veronicaMars, default).
+
+agent(cady).
+self_esteem(cady, 10).
+current_action(cady, default).
+
+agent(regina).
+self_esteem(regina, 10).
+current_action(regina, default).
+
+agent(janis).
+self_esteem(janis, 10).
+current_action(janis, default).
+
+agent(karen).
+self_esteem(karen,10).
+current_action(karen, default).
+
+agent(sharon).
+self_esteem(sharon, 10).
+current_action(sharon, default).
+
+agent(veronica).
+self_esteem(veronica, 10).
+current_action(veronica, default).
+
+agent(heather_duke).
+self_esteem(heather_duke, 10).
+current_action(heather_duke, default).
+
+agent(heather_chandler).
+self_esteem(heather_chandler, 10).
+current_action(heather_chandler, default).
+
+agent(heather_mcnamara).
+self_esteem(heather_mcnamara, 10).
+current_action(heather_mcnamara, default).
+
+
+generate_action_list([],[]).
+generate_action_list(Action_List, [Agent|Rest_Agents]):-
+	append([Agent:Action], Rest_Actions, Action_List),
+	choose_action(Agent, Action),
+	generate_action_list(Rest_Actions, Rest_Agents).
+
+execute_turn([], State, State).
+execute_turn([Agent:Action|Tail]):-
+	call(Action),
+	execute_turn(Tail).
+
+
+
+
+choose_action(Agent, attack(Agent, regina)).
+%%	obviously this will be more fully-fledged
+%       but that's kind of the crux of the game
+%	so i'm postponing it for now
+
+current_action(regina, defend).
+
+
+
+
+attack(Assailant, Victim):-
+  self_esteem(Assailant, _), self_esteem(Victim, Num),
+  (   current_action(Victim, defend) ->
+         New is Num - 1/2;
+    New is Num - 1),
+  update(self_esteem(Victim, Num), self_esteem(Victim, New)).
+
+
+
+%% affinity(+Person, -AffinityLevel)
+% True if SelfEsteem is the amount this character likes themselves.
+:- public affinity/2.
+affinity(Person, SelfEsteem) :-
+	random_float(0, 1, SelfEsteem),
+	asserta((affinity(Person, SelfEsteem):- !)),
+	!.
+
+:- public set_affinity/2.
+set_affinity(Person,Level) :-
+	person(Person),
+	(retract(affinity(Person, _)) ; true),
+	asserta((affinity(Person, Level):- !)),
+	!.
